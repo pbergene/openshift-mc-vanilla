3aed4ecdd93d98a1008c6c547c0927d89c18f8a9
Paul Bergene <paul@bergene.biz>
2017-09-03 00:44:49 +0200

    Screw it - PV for logs

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 oc new-app https://github.com/pbergene/openshift-mc-vanilla.git
 oc volume dc/openshift-mc-vanilla --add --claim-size 1G --mount-path /usr/games/minecraft/world --name minecraft-world
+oc volume dc/openshift-mc-vanilla --add --claim-size 1G --mount-path /usr/games/minecraft/logs --name minecraft-logs
