1d1017e7b1b01551f99d28d10e27bac9ffe0580f
Paul Bergene <paul@bergene.biz>
2017-09-03 00:28:00 +0200

    Deploy script "working" && c wd in run.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-oc volume dc/minecraft-world --add --claim-size 1G --mount-path /usr/games/minecraft/world --name minecraft-world
+
+oc new-app https://github.com/pbergene/openshift-mc-vanilla.git
+oc volume dc/openshift-mc-vanilla --add --claim-size 1G --mount-path /usr/games/minecraft/world --name minecraft-world
--- root/run.sh
+++ root/run.sh
@@ -4,4 +4,4 @@ id
 java -version
 echo "----"
 
-java -Xmx1024M -Xms1024M -jar /usr/games/minecraft/minecraft_server.1.12.1.jar nogui
+cd /usr/games/minecraft/ && java -Xmx1024M -Xms1024M -jar minecraft_server.1.12.1.jar nogui
